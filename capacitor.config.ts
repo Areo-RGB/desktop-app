671ab28f0886cb20164a86bb905ac005bc01bc7b
Areo-RGB <paziske@gmail.com>
2026-07-20 12:11:02 +0200

    sync

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.aistudioapplet',
+  appName: 'AI Studio Applet',
+  webDir: 'dist'
+};
+
+export default config;
